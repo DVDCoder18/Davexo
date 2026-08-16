c2b2a6b8c4c7b95659239873fad451bf9386c47c
DVDCoder18 <dvd17user@gmail.com>
2026-08-17 00:58:33 +0200

    feat(auth): define UserRepository

---
+++ davexo-backend/src/main/java/com/davexo/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.davexo.backend.repository;
+
+import com.davexo.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+    Optional<User> findByEmail(String email);
+
+}
