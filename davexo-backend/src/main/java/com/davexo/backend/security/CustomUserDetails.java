a28a566abee5c066c21f181a733f6f0b82f25f51
DVDCoder18 <dvd17user@gmail.com>
2026-08-17 11:43:41 +0200

    feat(auth): define user details and service

---
+++ davexo-backend/src/main/java/com/davexo/backend/security/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.davexo.backend.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.davexo.backend.entity.User;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+    
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return user.getIsActive();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
---
+++ davexo-backend/src/main/java/com/davexo/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.davexo.backend.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.davexo.backend.entity.User;
+import com.davexo.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé"));
+
+        return new CustomUserDetails(user);
+    }
+}
