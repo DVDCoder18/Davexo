af19502475ad35d136f37cfbf8688d8e2cd65974
DVDCoder18 <dvd17user@gmail.com>
2026-08-18 13:13:50 +0200

    feat(task_domain): define TaskRepository

---
+++ davexo-backend/src/main/java/com/davexo/backend/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.davexo.backend.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davexo.backend.entity.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    Optional<Task> findByIdAndUserId(Integer taskId, Integer userId);
+
+    List<Task> findAllByUserId(Integer userId);
+}
