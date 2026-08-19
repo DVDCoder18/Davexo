6ffbf66c3567160c995e4b4077a45efe05ac06f6
DVDCoder18 <dvd17user@gmail.com>
2026-08-19 09:59:53 +0200

    feat(expense_domain): implement service for Expense domain

---
+++ davexo-backend/src/main/java/com/davexo/backend/service/ExpenseService.java
@@ -0,0 +1,97 @@
+package com.davexo.backend.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.davexo.backend.dto.request.ExpenseRequestDto;
+import com.davexo.backend.dto.response.ExpenseResponseDto;
+import com.davexo.backend.entity.Expense;
+import com.davexo.backend.entity.ExpenseCategory;
+import com.davexo.backend.entity.User;
+import com.davexo.backend.exception.ResourceNotFoundException;
+import com.davexo.backend.mapper.ExpenseMapper;
+import com.davexo.backend.repository.ExpenseCategoryRepository;
+import com.davexo.backend.repository.ExpenseRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+    private final ExpenseCategoryRepository expenseCategoryRepository;
+    private final ExpenseMapper expenseMapper;
+
+    @Transactional
+    public ExpenseResponseDto addExpense(ExpenseRequestDto expenseRequestDto, User authenticatedUser) {
+        Expense expense = expenseMapper.toExpenseEntity(expenseRequestDto);
+
+        expense.setUser(authenticatedUser);
+
+        expense.setExpenseCategory(
+                expenseCategoryRepository.findByIdAndUserId(
+                        expenseRequestDto.getExpenseCategoryId(), authenticatedUser.getId())
+                        .orElseThrow(() -> new ResourceNotFoundException("Category Not found")));
+
+        if (expenseRequestDto.getExpenseDate() == null) {
+            expense.setExpenseDate(LocalDate.now());
+        }
+        Expense savedExpense = expenseRepository.save(expense);
+
+        return expenseMapper.toExpenseResponseDto(savedExpense);
+    }
+    
+    @Transactional
+    public ExpenseResponseDto updateExpense(ExpenseRequestDto expenseRequestDto, Integer expenseId, Integer userId) {
+        Expense expense = expenseRepository.findByIdAndUserId(expenseId, userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Expense not found"));
+
+        if (expenseRequestDto.getExpenseDate() != null) {
+            expense.setExpenseDate(expenseRequestDto.getExpenseDate());
+        }
+
+        ExpenseCategory expenseCategory = expenseCategoryRepository
+                .findByIdAndUserId(expenseRequestDto.getExpenseCategoryId(), userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+
+        expense.setLabel(expenseRequestDto.getLabel());
+        expense.setAmount(expenseRequestDto.getAmount());
+        expense.setNote(expenseRequestDto.getNote());
+        expense.setExpenseCategory(expenseCategory);
+
+        Expense savedExpense = expenseRepository.save(expense);
+
+        return expenseMapper.toExpenseResponseDto(savedExpense);
+
+    }
+
+    @Transactional 
+    public void deleteExpense(Integer expenseId, Integer userId) {
+
+        long deleteCount = expenseRepository.deleteByIdAndUserId(expenseId, userId);
+
+        if (deleteCount == 0) {
+            throw new ResourceNotFoundException("Expense not found");
+        }
+    }
+    
+    public ExpenseResponseDto getExpenseDetail(Integer expenseId, Integer userId) {
+
+        Expense expense = expenseRepository.findByIdAndUserId(expenseId, userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Expense not found"));
+
+        return expenseMapper.toExpenseResponseDto(expense);
+    }
+    
+    public List<ExpenseResponseDto> getAllExpenses(Integer userId) {
+        List<Expense> expenseList = expenseRepository.findAllByUserId(userId);
+
+        return expenseList.stream()
+                .map(expenseMapper::toExpenseResponseDto)
+                .toList();
+    }
+}
