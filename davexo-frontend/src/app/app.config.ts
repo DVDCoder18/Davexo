b6bcb92d8d0b68b713e2c0b6826f36ed77522b02
DVDCoder18 <dvd17user@gmail.com>
2026-08-22 13:38:15 +0200

    add Typscript Angular client generated with OpenAPI Generator + basic configs for use of codex in this project

--- davexo-frontend/src/app/app.config.ts
+++ davexo-frontend/src/app/app.config.ts
@@ -1,10 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
+import { provideApi } from './api/provide-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+
+    provideHttpClient(),
+
+    provideRouter(routes),
+
+    provideApi({
+      basePath: 'http://localhost:8081'
+    })
   ]
 };
